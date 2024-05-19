5595afc68ccc7e1df5d78597afcc42b2af76e4a4
Saucy_Salad <andrewbruce222@gmail.com>
2024-05-20 06:14:48 +1000

    github-actions build

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     basePath: "/circles-profile",
-    output: "export",  // <=== enables static exports
+    output: "export",
     reactStrictMode: true,
 };
 
